6cf86f374a8bfacd26e2912a0b6d07368cd68f1f
patricktelnoni <patricktelnoni@users.noreply.github.com>
2024-10-11 14:22:09 +0700

    Add files via upload

---
+++ Perulangan.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Perulangan{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int angka;
+        int hasilAkhir = 0;
+        angka  = input.nextInt();
+        // Start pengerjaan soal
+
+
+        // End Pengerjaan soal
+
+        input.close();
+        System.out.println(hasilAkhir);
+
+    }
+}
